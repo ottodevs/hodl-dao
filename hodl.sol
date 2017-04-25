0778359a5fcbbd98bbc9d9efd9fe0b4820234922
CurrencyTycoon <flashmob+currencytycoon@gmail.com>
2017-04-26 09:41:20 +1000

    changed to reflect verified source code https://etherscan.io/address/0x6b3Da034ebAD473aCb93729a7152c672cC0DC6cd#code

--- hodl.sol
+++ hodl.sol
@@ -10,37 +10,37 @@ pragma solidity ^0.4.10;
 *
 */
 contract HodlDAO {
-    /* ERC20 Public variables of the token */
+/* ERC20 Public variables of the token */
     string public version = 'HDAO 0.2';
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
 
-    /* ERC20 This creates an array with all balances */
+/* ERC20 This creates an array with all balances */
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
 
 
-    /* store the block number when a withdrawal has been requested*/
+/* store the block number when a withdrawal has been requested*/
     mapping (address => withdrawalRequest) public withdrawalRequests;
     struct withdrawalRequest {
     uint sinceBlock;
     uint256 amount;
     }
 
-    /**
-     * feePot collects fees from quick withdrawals. This gets re-distributed to slow-withdrawals
-    */
+/**
+ * feePot collects fees from quick withdrawals. This gets re-distributed to slow-withdrawals
+*/
     uint256 public feePot;
 
-    //uint32 public constant blockWait = 172800; // roughly 30 days,  (2592000 / 15) - assuming block time is ~15 sec.
-    uint public constant blockWait = 8; // roughly assuming block time is ~15 sec.
+    uint32 public constant blockWait = 172800; // roughly 30 days,  (2592000 / 15) - assuming block time is ~15 sec.
+//uint public constant blockWait = 8; // roughly assuming block time is ~15 sec.
 
 
-    /**
-     * ERC20 events these generate a public event on the blockchain that will notify clients
-    */
+/**
+ * ERC20 events these generate a public event on the blockchain that will notify clients
+*/
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
@@ -51,11 +51,11 @@ contract HodlDAO {
     event WithdrawalPremature(address indexed by, uint blocksToWait); // Needs to wait blocksToWait before withdrawal unlocked
     event Deposited(address indexed by, uint256 amount);
 
-    /**
-     * Initializes contract with initial supply tokens to the creator of the contract
-     * In our case, there's no initial supply. Tokens will be created as ether is sent
-     * to the fall-back function. Then tokens are burned when ether is withdrawn.
-     */
+/**
+ * Initializes contract with initial supply tokens to the creator of the contract
+ * In our case, there's no initial supply. Tokens will be created as ether is sent
+ * to the fall-back function. Then tokens are burned when ether is withdrawn.
+ */
     function HodlDAO(
     uint256 initialSupply,
     string tokenName,
@@ -70,22 +70,22 @@ contract HodlDAO {
         decimals = decimalUnits;                            // Amount of decimals for display purposes
     }
 
-    /**
-     * notPendingWithdrawal modifier guards the function from executing when a
-     * withdrawal has been requested and is currently pending
-     */
+/**
+ * notPendingWithdrawal modifier guards the function from executing when a
+ * withdrawal has been requested and is currently pending
+ */
     modifier notPendingWithdrawal {
         if (withdrawalRequests[msg.sender].sinceBlock > 0) throw;
         _;
     }
 
 
-    /** ERC20 - transfer sends tokens
-     * @notice send `_value` token to `_to` from `msg.sender`
-     * @param _to The address of the recipient
-     * @param _value The amount of token to be transferred
-     * @return Whether the transfer was successful or not
-     */
+/** ERC20 - transfer sends tokens
+ * @notice send `_value` token to `_to` from `msg.sender`
+ * @param _to The address of the recipient
+ * @param _value The amount of token to be transferred
+ * @return Whether the transfer was successful or not
+ */
     function transfer(address _to, uint256 _value) notPendingWithdrawal {
         if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
         if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
@@ -94,12 +94,12 @@ contract HodlDAO {
         Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
     }
 
-    /** ERC20 approve allows another contract to spend some tokens in your behalf
-     * @notice `msg.sender` approves `_spender` to spend `_value` tokens
-     * @param _spender The address of the account able to transfer the tokens
-     * @param _value The amount of tokens to be approved for transfer
-     * @return Whether the approval was successful or not
-     */
+/** ERC20 approve allows another contract to spend some tokens in your behalf
+ * @notice `msg.sender` approves `_spender` to spend `_value` tokens
+ * @param _spender The address of the account able to transfer the tokens
+ * @param _value The amount of tokens to be approved for transfer
+ * @return Whether the approval was successful or not
+ */
     function approve(address _spender, uint256 _value) notPendingWithdrawal
     returns (bool success) {
         allowance[msg.sender][_spender] = _value;
@@ -107,9 +107,9 @@ contract HodlDAO {
     }
 
 
-    /**
-     * ERC-20 Approves and then calls the receiving contract
-    */
+/**
+ * ERC-20 Approves and then calls the receiving contract
+*/
     function approveAndCall(address _spender, uint256 _value, bytes _extraData) notPendingWithdrawal
     returns (bool success) {
         allowance[msg.sender][_spender] = _value;
@@ -122,14 +122,14 @@ contract HodlDAO {
         return true;
     }
 
-    /**
-     * ERC20 A contract attempts to get the coins
-     * @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
-     * @param _from The address of the sender
-     * @param _to The address of the recipient
-     * @param _value The amount of token to be transferred
-     * @return Whether the transfer was successful or not
-     */
+/**
+ * ERC20 A contract attempts to get the coins
+ * @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+ * @param _from The address of the sender
+ * @param _to The address of the recipient
+ * @param _value The amount of token to be transferred
+ * @return Whether the transfer was successful or not
+ */
     function transferFrom(address _from, address _to, uint256 _value)  notPendingWithdrawal
     returns (bool success) {
         if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
@@ -142,27 +142,24 @@ contract HodlDAO {
         return true;
     }
 
-    /**
-     * withdrawalInitiate initiates the withdrawal by going into a waiting period
-     * It remembers the block number & amount held at the time of request.
-     * After the waiting period finishes, the call withdrawalComplete
-     *
-     * Gas 64490
-     *
-    */
+/**
+ * withdrawalInitiate initiates the withdrawal by going into a waiting period
+ * It remembers the block number & amount held at the time of request.
+ * After the waiting period finishes, the call withdrawalComplete
+ */
     function withdrawalInitiate() notPendingWithdrawal {
         WithdrawalStarted(msg.sender, balanceOf[msg.sender]);
         withdrawalRequests[msg.sender] = withdrawalRequest(block.number, balanceOf[msg.sender]);
     }
 
-    /**
-     * withdrawalComplete is called after the waiting period. The ether will be
-     * returned to the caller and the tokens will be burned.
-     * A reward will be issued based on the amount in the feePot relative to the
-     * amount held when the withdrawal request was made.
-     *
-     * Gas: 125579
-    */
+/**
+ * withdrawalComplete is called after the waiting period. The ether will be
+ * returned to the caller and the tokens will be burned.
+ * A reward will be issued based on the amount in the feePot relative to the
+ * amount held when the withdrawal request was made.
+ *
+ * Gas: 17008
+ */
     function withdrawalComplete() returns (bool) {
         withdrawalRequest r = withdrawalRequests[msg.sender];
         if (r.sinceBlock == 0) throw;
@@ -188,9 +185,9 @@ contract HodlDAO {
 
     }
 
-    /**
-     * Reward is based on the amount held, relative to total supply of tokens.
-     */
+/**
+ * Reward is based on the amount held, relative to total supply of tokens.
+ */
     function calculateReward(uint256 v) constant returns (uint256) {
         uint256 reward = 0;
         if (feePot > 0) {
@@ -199,18 +196,18 @@ contract HodlDAO {
         return reward;
     }
 
-    /** calculate the fee for quick withdrawal
-     */
+/** calculate the fee for quick withdrawal
+ */
     function calculateFee(uint256 v) constant returns  (uint256) {
         uint256 feeRequired = v / (1 wei * 100);
         return feeRequired;
     }
 
-    /**
-     * Quick withdrawal, needs to send ether to this function for the fee.
-     *
-     * Gas use: 84235 (including call to processWithdrawal)
-    */
+/**
+ * Quick withdrawal, needs to send ether to this function for the fee.
+ *
+ * Gas use: 44129 (including call to processWithdrawal)
+*/
     function quickWithdraw() payable notPendingWithdrawal returns (bool) {
     // calculate required fee
         uint256 amount = balanceOf[msg.sender];
@@ -235,10 +232,10 @@ contract HodlDAO {
         return true;
     }
 
-    /**
-     * do withdrawal
-     * Gas: 62483
-     */
+/**
+ * do withdrawal
+ * Gas: 62483
+ */
     function doWithdrawal(uint256 extra) internal {
         uint256 amount = balanceOf[msg.sender];
 
@@ -254,10 +251,10 @@ contract HodlDAO {
     }
 
 
-    /**
-    * Fallback function when sending ether to the contract
-     * Gas use: 1086390
-    */
+/**
+ * Fallback function when sending ether to the contract
+ * Gas use: 65051
+*/
     function () payable notPendingWithdrawal {
         uint256 amount = msg.value;  // amount that was sent
         if (amount <= 0) throw; // need to send some ETH
